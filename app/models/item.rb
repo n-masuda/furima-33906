3106be042f24e3b40be48690d046642288a5f144
n-masuda <m7241@me.com>
2021-02-10 18:04:49 +0900

    itemモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :price, presence: true
+  validates :shipment_id, presence: true
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, delivery_fee_id, :prefecture_id, :price, :shipment_id, :image).merge(user_id: current_user.id)
+  end
 end
