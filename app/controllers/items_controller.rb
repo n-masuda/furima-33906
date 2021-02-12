ba264afd602aefbe0ab3de0d541ef4adda16a267
n-masuda <m7241@me.com>
2021-02-12 15:26:34 +0900

    items/index商品表示の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
@@ -15,6 +16,10 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+# def show
+    
+ # end
 
   private
   def item_params
