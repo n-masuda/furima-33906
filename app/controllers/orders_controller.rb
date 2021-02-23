b0fc5c84eff50c78fd95736acc3c87427d23f246
n-masuda <m7241@me.com>
2021-02-24 00:38:15 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,26 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  #before_action :own_user
+  before_action :own_user!
   def index
     @form = Form.new
   end
 
   def create
-
     @form = Form.new(form_params)
     if @form.valid?
+      pay_item
       @form.save
       redirect_to root_path
     else
-      render :index
+      return render :index
     end
   end
 
 
   private
   def form_params
-    params.require(:form).permit(:postal_code, :city, :add_line1, :add_line2, :phone, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:form).permit(:postal_code, :city, :add_line1, :add_line2, :phone, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -32,4 +32,14 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: form_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,13 +1,14 @@
 class Form
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :city, :add_line1, :add_line2, :phone, :prefecture_id, :order_id
+  attr_accessor :user_id, :item_id, :postal_code, :city, :add_line1, :add_line2, :phone, :prefecture_id, :order_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "incorrect" }
     validates :city
     validates :add_line1
-    validates :phone, numericality: { only_integer:true,less_than:12} 
+    validates :phone, length: {maximum: 11}, numericality: { only_integer:true} 
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank"}
+    validates :token
   end
 
   def save
